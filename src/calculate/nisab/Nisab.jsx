0c3653e4c3ad1ef9e86def308f9d5a2cdbddd2b2
Shubhra-dev <shuuvro.dev@gmail.com>
2024-04-25 18:47:58 +0600

    Today's nisab hero section, daily rates updated

--- src/calculate/nisab/Nisab.jsx
+++ src/calculate/nisab/Nisab.jsx
@@ -3,6 +3,7 @@ import CalculateLaoyout from "../CalculateLaoyout";
 import NisabHero from "./NisabHero";
 import TodaysNisab from "./TodaysNisab";
 import nisabQues from "../../assets/nisabQues.png";
+import { FaAngleDoubleRight, FaAngleDoubleUp } from "react-icons/fa";
 
 function Nisab() {
   return (
@@ -33,6 +34,28 @@ function Nisab() {
             <h2 className="text-secondary font-semibold text-6xl font-pally pb-4">
               Nisab Related Questions
             </h2>
+            <div className="px-3 border border-gray-200 py-4 shadow-[1px_1px_25px_1px_rgba(0,0,0,0.15)] rounded-2xl flex items-center cursor-pointer">
+              <div className="rounded-full w-max p-2 bg-secondary/20 text-2xl text-accentRed">
+                <FaAngleDoubleRight className="text-xl" />
+              </div>
+              <h3 className="text-3xl font-semibold text-secondary px-4">
+                What is Nisab?
+              </h3>
+            </div>
+            <div className="px-3 bg-accentRed border border-gray-200 py-4 shadow-[1px_1px_25px_1px_rgba(0,0,0,0.15)] rounded-2xl flex items-center cursor-pointer">
+              <div className="rounded-full w-max p-2 bg-primary text-2xl text-accentRed">
+                <FaAngleDoubleUp className="text-xl" />
+              </div>
+              <div className="px-4 text-primary">
+                <h3 className="text-3xl font-semibold pb-2">What is Nisab?</h3>
+                <p className="font-normal text-sm">
+                  The Nisab was set by the Prophet Muhammad (peace be upon him)
+                  at a rate equivalent to87.48 grams of gold and 612.36 grams of
+                  silver (85 grams of gold and 595 grams of silver according is
+                  another opinion).
+                </p>
+              </div>
+            </div>
           </div>
         </EqualHeightFlexContainer>
       </div>
