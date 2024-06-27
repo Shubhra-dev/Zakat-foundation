9b0923f4a4eeadfbeb4958bbbd93378256f80acf
Shubhra-dev <shuuvro.dev@gmail.com>
2024-06-27 23:41:29 +0600

    your impact page hero section responsive done for android

--- src/impact/ImpactLayout.jsx
+++ src/impact/ImpactLayout.jsx
@@ -1,5 +1,4 @@
-import EqualHeightFlexContainer from "../ui/EqualHeightFlexContainer";
-
+//import EqualHeightFlexContainer from "../ui/EqualHeightFlexContainer";
 import MenuItem from "../calculate/MenuItem";
 import { PiFlowerBold } from "react-icons/pi";
 import { GiRecycle } from "react-icons/gi";
@@ -8,8 +7,8 @@ import { LuHeartHandshake } from "react-icons/lu";
 
 function ImpactLayout({ children, tab }) {
   return (
-    <EqualHeightFlexContainer>
-      <div className="w-1/6 bg-slate-100 px-10 pt-8 h-full flex-grow">
+    <div className="sm:h-screen sm:flex">
+      <div className="overflow-x-scroll flex sm:block gap-2 sm:w-1/6 bg-slate-100 px-2 sm:px-10 pt-3 sm:pt-8 sm:h-full flex-grow sm:overflow-y-scroll scrollbar-hide">
         <MenuItem text={"Your Impact"} active={tab === 1} link={"your_impact"}>
           <LuHeartHandshake
             className={`text-2xl ${
@@ -47,8 +46,10 @@ function ImpactLayout({ children, tab }) {
           />
         </MenuItem>
       </div>
-      <div className="w-5/6 flex-grow">{children}</div>
-    </EqualHeightFlexContainer>
+      <div className="w-full h-full sm:w-5/6 sm:flex-grow overflow-y-scroll scrollbar-hide">
+        {children}
+      </div>
+    </div>
   );
 }
 
